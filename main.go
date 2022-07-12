f12f9925f0184d778acfab5341e6a919079a5455
Vanshaj <79518089+V4NSH4J@users.noreply.github.com>
2022-07-12 19:30:31 +0530

    Forgot to update Version
    1.10.6 -> 1.10.7

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 var CaptchaServices []string
 
 func main() {
-	version := "1.10.6"
+	version := "1.10.7"
 	rand.Seed(time.Now().UTC().UnixNano())
 	color.Blue.Printf(logo + " v" + version + "\n")
 	color.Green.Printf("Made by https://github.com/V4NSH4J\nStar repository on github for updates!\n")
